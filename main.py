daa01c9cb06e79a4f2918ce736283bf1aa0bdeed
Juan Arismendy TS <juanarismendy@truckstop.com>
2024-11-10 11:32:34 -0500

    add ValueError exception

--- main.py
+++ main.py
@@ -3,23 +3,26 @@ import parser as AS
 
 
 while True:
-    userInput = input("Ingrese una operación simple: ")
-    Lexer = AL.LexicalAnalyzer(userInput)
-    Lexer.generarTokens()
-    tokens = Lexer.getListaTokens()
-
-    print("Estos son los tokens generados por el analizador léxico ")
-    print(tokens)
-    print(
-        "Este es el resultado del análisis sintáctico de los tokens(si retorna un número, es una cadena válida)"
-    )
     try:
+        userInput = input("\nIngrese una operación simple: ")
+        Lexer = AL.LexicalAnalyzer(userInput)
+        Lexer.generarTokens()
+        tokens = Lexer.getListaTokens()
+
+        print("Tokens generados por el analizador léxico: ")
+        print(tokens)
+        print(
+            "Resultado (si retorna un número, es una cadena válida):"
+        )
+    
         Parser = AS.Parser(tokens)
         print(Parser.parse())
     except ZeroDivisionError:
         print("Error matemático, división entre cero")
-    except Exception:
-        print("Expresión Inválida")
+    except ValueError as e:
+        print(e)
+    except Exception as e:
+        print("Expresión Inválida: ", e)
     print("Desea ingresar otra operación? (s/n)")
     wantToContinue = input()
     if wantToContinue == "n":
